67fdf75bae154ecd270fd8a587531951db0fbb02
Kleaxaxin <saketm1@gmail.com>
2016-09-28 21:02:03 -0400

    Add files via upload

--- Caesar.java
+++ Caesar.java
@@ -1,24 +1,29 @@
 package net.mrpaul.ADSA.WM110.ps04;
-// letters r 65-91 for uppercase and 97-123 for lowercase in ascii
+
 import java.util.Scanner;
 
 public class Caesar {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		if (args.length > 0) {
+			
 			int key = Integer.parseInt(args[0]) % 26;
 			int asciiMessage;
 			Scanner reader = new Scanner(System.in);
 			System.out.print("Please enter a message to encrypt: ");
 			String message = reader.nextLine();
+			
 			for (int i = 0; i < message.length(); i++){
 				asciiMessage = (int) message.charAt(i);
 				if ((asciiMessage <= 91 && asciiMessage >= 65) || (asciiMessage <= 123 && asciiMessage >= 97)){
-					
+					asciiMessage += 1;
+					System.out.print((char) asciiMessage);
+				} 
+				else {
+					System.out.print(message.charAt(i));	
 				}
 			}
 		}
 	}
-
 }
